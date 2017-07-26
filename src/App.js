c8ae1ccd7465267ca4627db75ffd32bb5bc9611c
Mike Moran <mike@houseofmoran.com>
2017-07-26 16:03:25 +0200

    add Sett editor to App
    also:
    - remove unused imports
    - Sett parsing: ignore warning about assignment within while
    - bug fix: set width and sett on Shartan

--- src/App.js
+++ src/App.js
@@ -4,13 +4,30 @@ import './App.css';
 import Shartan from '../src/Shartan';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Shartan />
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            sett: "K4 R24 K9 Y4",
+        };
+
+        this.handleSettChange = this.handleSettChange.bind(this);
+    }
+
+    handleSettChange(event) {
+        this.setState({sett: event.target.value});
+    }
+
+    render() {
+      return (
+        <div className="App">
+          <Shartan sett={this.state.sett} width={400}/>
+          <input style={{position: "relative", top: 10, left: 10, zIndex: 1}}
+                 type="text"
+                 value={this.state.sett}
+                 onChange={this.handleSettChange} />
+        </div>
+      );
+    }
 }
 
 export default App;
