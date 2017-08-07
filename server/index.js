3950820abd4dbae7e52cbb4b43eccf7a85fc369f
Mike Moran <mike@houseofmoran.com>
2017-08-07 19:03:14 +0200

    get client

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import CDP from 'chrome-remote-interface';
 import express from 'express';
 import path from 'path';
 import morgan from 'morgan';
@@ -7,19 +8,29 @@ const app = express();
 app.use(morgan('combined'));
 app.use('/shartan', express.static(path.resolve(__dirname + '/../../build')));
 
-app.get('/preview.png', (req, res) => {
+app.get('/preview.png', async (req, res) => {
+    const client = await CDP();
+    console.log("got client");
+
+    const {Network, Page} = client;
+
     const base64Img = "iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAAnElEQVR42u3" +
         "RAQ0AAAgDIE1u9FvDOahApzLFGS1ECEKEIEQIQoQgRIgQIQgRghAhCBGCECEIQYgQhAhBiBCE" +
         "CEEIQoQgRAhChCBECEIQIgQhQhAiBCFCEIIQIQgRghAhCBGCEIQIQYgQhAhBiBCEIEQIQoQgR" +
         "AhChCAEIUIQIgQhQhAiBCEIEYIQIQgRghAhCBEiRAhChCBECEK+W99M+TnxqRsqAAAAAElFTk" +
         "SuQmCC";
     const img = Buffer.from(base64Img, "base64");
+    console.log("created image");
 
     res.writeHead(200, {
         'Content-Type': 'image/png',
         'Content-Length': img.length
     });
     res.end(img);
+    console.log("sent image");
+
+    client.close();
+    console.log("closed client");
 });
 
 const port = 3002;
